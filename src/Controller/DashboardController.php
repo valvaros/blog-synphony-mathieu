bdd269a4e2f1332a0c66e52d6b2cb445cf024748
valvaros <mathieu.guezennec@hotmail.com>
2021-12-23 11:59:11 +0100

    Authenticated roles et accescontrol

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -7,11 +7,16 @@ use App\Entity\Article;
 use App\Entity\Category;
 use App\Form\EditArticleType;
 use Doctrine\ORM\EntityManagerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+/**
+ * Pour limiter l'accés à tous le controller et les routes qu'il contient.
+ * @IsGranted("ROLE_ADMIN")
+ */
 class DashboardController extends AbstractController
 {
     public function __construct(EntityManagerInterface $entityManager)
@@ -41,6 +46,8 @@ class DashboardController extends AbstractController
     }
 
     /**
+     * Pour limiterl'accès à une route ou action en particulier à un rôle.
+     * @iSgranted("ROLE_SUPER_ADMIN")
      * @Route("/admin/supprimer/user/{id}", name="delete_user")
      * @param User $user
      * @return Response
